300815fc78c692e7529b45b49a0c996bc308c627
sinan cengiz <sinancengiz_84@hotmail.com>
2020-10-08 15:52:28 -0700

    email column added to users model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def create   
-    @user = User.create(params.require(:user).permit(:username, :password))   
+    @user = User.create(params.require(:user).permit(:username, :password, :email))   
     session[:user_id] = @user.id   
     redirect_to '/welcome'
   end
