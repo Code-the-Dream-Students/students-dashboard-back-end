745a44c5e62ba92fadf37fbda04ffa440d2afd38
sinan cengiz <sinancengiz_84@hotmail.com>
2020-10-08 12:15:10 -0700

    bcrypt library installed  user and sessions models and controller created and login sign up and logout functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]   
+  get 'login', to: 'sessions#new'   
+  post 'login', to: 'sessions#create'   
+  get 'welcome', to: 'sessions#welcome'
+  
   root 'students#index'
   get '/mentors', to: 'mentors#index'
+
+  get 'authorized', to: 'sessions#page_requires_login'
+
+  get '/logout', to: 'sessions#logout', as: 'logout'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
