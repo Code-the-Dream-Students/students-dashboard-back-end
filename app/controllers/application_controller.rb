a384776ef850a15531b9cbfa7f27ca77a8c0a17b
Denilson V <deethedev@gmail.com>
2020-10-29 20:31:09 -0400

    Cookies workflow created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::API
+    include ::ActionController::Cookies
     include Response
     include ExceptionHandler
 
     # called before every action on controllers
-  before_action :authorize_request
+  # before_action :authorize_request
+  before_action :authenticate_user
   attr_reader :current_user
 
   private
@@ -11,5 +13,10 @@ class ApplicationController < ActionController::API
   # Check for valid request token and return user
   def authorize_request
     @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
+  def authenticate_user
+    jwt = cookies.signed[:jwt]
+    render json: {message: 'Please Login - from authenticate_user'}, status: :unauthorized unless jwt
+    # decode_jwt(jwt)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,9 @@ module StudentsDashboardBackEnd
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    # config.api_only = true
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
