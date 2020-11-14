aee678ad79575e0aa75e73cf80360baca40a5000
sinan cengiz <sinancengiz_84@hotmail.com>
2020-11-14 11:48:21 -0800

    exception handler

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -14,27 +14,26 @@
 #     end
 #   end
 
-
   module ExceptionHandler
+    # provides the more graceful `included` method
     extend ActiveSupport::Concern
-  
+    
     # Define custom error subclasses - rescue catches `StandardErrors`
     class AuthenticationError < StandardError; end
     class MissingToken < StandardError; end
     class InvalidToken < StandardError; end
   
     included do
-      # Define custom handlers
-  
-      rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
-      rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
-      rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
-      rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
       rescue_from ActiveRecord::RecordNotFound do |e|
         json_response({ message: e.message }, :not_found)
       end
-    end
   
+      rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+      rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+      rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+      rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+  
+    end
     private
   
     # JSON response with message; Status code 422 - unprocessable entity
