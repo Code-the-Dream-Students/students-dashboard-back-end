0968fe30df48dcb881a5533efc348c0f620dfdd8
Denilson V <deethedev@gmail.com>
2020-11-17 00:24:06 -0500

    Added cookie to user.create and :role params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 # app/controllers/users_controller.rb
 class UsersController < ApplicationController
 
-    skip_before_action :authenticate_user, only: :create
+    skip_before_action :authorize_user, only: :create
     # POST /signup
     # return authenticated token upon signup
     def create
       user = User.create!(user_params)
       auth_token = AuthenticateUser.new(user.email, user.password).call
+      # HTTP-only cookie stored with refresh_token
+      cookies.signed[:jwt] = {value:  auth_token, httponly: true, expires: 2.hours.from_now}
+          
       response = { message: Message.account_created, auth_token: auth_token }
       json_response(response, :created)
     end
@@ -23,6 +26,7 @@ class UsersController < ApplicationController
       params.permit(
         :username,
         :email,
+        :role,
         :password,
         :password_confirmation
       )
