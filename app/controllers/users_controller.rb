34141c6840c5d5e197d4eb359d065fc16fbe5b12
Denilson V <deethedev@gmail.com>
2020-11-17 02:11:27 -0500

    Configure logout function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,35 @@
 # app/controllers/users_controller.rb
 class UsersController < ApplicationController
-
-    skip_before_action :authorize_user, only: :create
-    # POST /signup
-    # return authenticated token upon signup
-    def create
-      user = User.create!(user_params)
-      auth_token = AuthenticateUser.new(user.email, user.password).call
-      # HTTP-only cookie stored with refresh_token
-      cookies.signed[:jwt] = {value:  auth_token, httponly: true, SameSite: "Strict", expires: 2.hours.from_now}
-          
-      response = { message: Message.account_created, auth_token: auth_token }
-      json_response(response, :created)
-    end
-
-    def logout
-      current_user && current_user.a 
-      json_response(response, :created)
-    end
-
-  
-    private
-  
-    def user_params
-      params.permit(
-        :username,
-        :email,
-        :role,
-        :password,
-        :password_confirmation
-      )
-    end
+  skip_before_action :authorize_user, only: :create
+  # POST /signup
+  # return authenticated token upon signup
+  def create
+    user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.email, user.password).call
+    # HTTP-only cookie stored with refresh_token
+    cookies.signed[:jwt] = {value:  auth_token, httponly: true, SameSite: "Strict", expires: 2.hours.from_now}
+        
+    response = { message: Message.account_created, auth_token: auth_token }
+    json_response(response, :created)
   end
+
+  def logout
+    # Delete cookie to invalidate session
+    cookies.delete :jwt
+    json_response(message: Message.logout_success)
+  end
+end
+
+
+  private
+
+  def user_params
+    params.permit(
+      :username,
+      :email,
+      :role,
+      :password,
+      :password_confirmation
+    )
+  end
+end
