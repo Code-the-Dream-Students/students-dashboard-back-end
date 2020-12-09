61cea04c5113e074cb6e408f203e702c2cd28608
Denilson V <deethedev@gmail.com>
2020-12-08 21:30:23 -0500

    Configured rack cors

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module StudentsDashboardBackEnd
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000', ' 127.0.0.1:3000',
+        origins 'localhost:3000',
         resource(
           'http://localhost:3000/',
           headers: :any,
