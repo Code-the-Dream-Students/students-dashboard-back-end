9b0f2903aeb766d7a9687ebe40b9ca4701977552
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-14 13:46:47 -0800

    Changes for solving bug in Heroku

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,6 +1,6 @@
 class WeeksController < ApplicationController
 
-  # skip_before_action :authenticate_user
+  skip_before_action :authenticate_user
 
   # before_action :set_week, only: [:show, :update, :destroy]
 
