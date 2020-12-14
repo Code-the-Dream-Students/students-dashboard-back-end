ba00f986306d3f259a74f15a1c4b5ab5458ee370
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-14 13:43:06 -0800

    Changes for solving bug in Heroku

--- app/controllers/lesson_sources_controller.rb
+++ app/controllers/lesson_sources_controller.rb
@@ -1,6 +1,6 @@
 class LessonSourcesController < ApplicationController
 
-  # skip_before_action :authenticate_user
+  skip_before_action :authenticate_user
 
   def create
     if set_lesson_source == nil
