e65b1941e01ad379e039bb9c2df3cd9d8f7982e4
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-15 14:37:43 -0800

    Setting domains for deployment 5

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -11,7 +11,8 @@ class AuthenticationController < ApplicationController
         httponly: true, 
         # same_site: :none,
         secure: true,
-        domain: 'localhost:3001', 
+        # domain: 'localhost:3001', 
+        domain: :all, 
         expires: 2.hours.from_now}
                 
       json_response(auth_token: auth_token, user_role: user.role,user_id: user.id)
