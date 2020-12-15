5c7c424ab06da9647e736eb3276295bba7b2cb80
Denilson V <deethedev@gmail.com>
2020-12-14 21:09:24 -0500

    Removed SameSite"strict" from cookie

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,7 +6,8 @@ class AuthenticationController < ApplicationController
       auth_token = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
       user = User.find_by(email: auth_params[:email])
       # HTTP-only cookie stored with refresh_token
-      cookies.signed[:jwt] = {value:  auth_token, httponly: true, SameSite: "Strict", expires: 2.hours.from_now}
+      # Note - May be needed before launching production:  SameSite: "Strict"
+      cookies.signed[:jwt] = {value:  auth_token, httponly: true, expires: 2.hours.from_now}
                 
       json_response(message: "Successfully authenticated." , user_role: user.role,user_id: user.id)
     end
