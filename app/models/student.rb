4edf46025d6562dad3863d79485a13bf8851a5bd
Denilson V <deethedev@gmail.com>
2020-12-14 21:09:35 -0500

    Added validation to Student Model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,9 @@
 class Student < ApplicationRecord
+  # Model associations
   belongs_to :user
+
+# Validations
+  # validates :role, presence: true
+
+  validates_presence_of :first_name, :last_name, :user
 end
