18e177e323a65e8ae56cd3c169302c3ac5f9c7af
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-22 23:25:51 -0800

    Create and Delete methods set up in mentor_courses controller Routes set up for Create and Delete methods of mentor_courses

--- app/controllers/mentor_courses_controller.rb
+++ app/controllers/mentor_courses_controller.rb
@@ -27,4 +27,53 @@ class MentorCoursesController < ApplicationController
     end
   end
 
+  def create
+    if current_user.role == "mentor"
+      if (current_user.mentor.courses.filter { |c| c.id == params[:course_id] }).length == 0 
+        mentor_course = MentorCourse.new(
+          mentor_id: current_user.mentor.id,
+          course_id: params[:course_id]
+        )
+        if mentor_course.save
+          render ({
+            json: { 
+              message: "Course added to mentor",
+              course: mentor_course 
+            }, 
+            status: 201
+          })
+        else
+          render json: { error: "Unprocessable Entity" }, status: 422
+        end
+      else
+        render json: { error: "Mentor already has that course." }, status: 422
+      end
+    else
+      render json: { error: "Not Found" }, status: 404
+    end
+  end
+
+  def destroy
+    if current_user.role === "mentor"
+      mentor_course = MentorCourse.find_by(
+        mentor_id: current_user.mentor.id, 
+        course_id: params[:course_id]
+      )
+      
+      if mentor_course.destroy
+        render ({
+          json: {
+            message: "Course deleted in mentor",
+            course: mentor_course
+          }, 
+          status: 200
+        })
+      else
+        render json: { error: "Unable to destroy course in mentor" }, status: 422
+      end
+    else
+      render json: { error: "Not Found" }, status: 404
+    end
+  end
+
 end
