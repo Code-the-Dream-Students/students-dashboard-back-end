fe8d6fbbac56fe9cb95f1cb1f25fb91ea465efcf
Jorge Gabitto <jgabitto1792@gmail.com>
2021-01-06 17:04:14 -0600

    removed migrations corresponding to student_courses

--- db/migrate/20201224153615_remove_details_from_student_course.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDetailsFromStudentCourse < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :student_courses, :course_id, :integer
-    remove_column :student_courses, :student_id, :integer
-  end
-end
