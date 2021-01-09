9076fd2b9d146ed3beebaa08a28d99705b4254a8
Jorge Gabitto <jgabitto1792@gmail.com>
2021-01-09 15:42:07 -0600

    Uncommented methods mentor_course_serializer

--- app/serializers/mentor_course_serializer.rb
+++ app/serializers/mentor_course_serializer.rb
@@ -1,5 +1,5 @@
 class MentorCourseSerializer < ActiveModel::Serializer
-  attributes :message, :id, :day, :start_time, :end_time
+  attributes :message, :id, :day, :start, :end
 
   belongs_to :mentor
   belongs_to :course
@@ -8,10 +8,10 @@ class MentorCourseSerializer < ActiveModel::Serializer
     message = "Success"
   end
 
-  # def start
-  #   object.start_time.to_time.strftime("%I:%M%P")
-  # end
-  # def end
-  #   object.end_time.to_time.strftime("%I:%M%P")
-  # end
+  def start
+    object.start_time.to_time.strftime("%I:%M%P")
+  end
+  def end
+    object.end_time.to_time.strftime("%I:%M%P")
+  end
 end
