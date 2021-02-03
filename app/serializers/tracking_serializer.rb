f21094e20ab29987e2f5ff2cedcf6134477d398d
Jorge Gabitto <jgabitto1792@gmail.com>
2021-02-03 09:36:04 -0600

    added units key to tracking_serializer

--- app/serializers/tracking_serializer.rb
+++ app/serializers/tracking_serializer.rb
@@ -1,5 +1,5 @@
 class TrackingSerializer < ActiveModel::Serializer
-  attributes :student_id, :course_id, :course, :first_name, :last_name, :student_weekly_progress
+  attributes :student_id, :user_id, :course_id, :course, :first_name, :last_name, :units, :student_weekly_progress
 
   # has_many :student_weekly_progresses
   # belongs_to :week
@@ -24,16 +24,20 @@ class TrackingSerializer < ActiveModel::Serializer
     object.id
   end
 
+  def user_id
+    object.user.id
+  end
+
   def student_weekly_progress
     object.student_weekly_progresses.map do |progress|
       {
+        week: progress.week_number,
+        week_id: progress.week.id,
         unit_id: progress.week.unit.id,
         unit: progress.week.unit.unit_name,
         unit_description: progress.week.unit.description,
         lesson_id: progress.week.lesson.id,
         lesson: progress.week.lesson.lesson_name,
-        week: progress.week_number,
-        week_id: progress.week.id,
         total_progress: progress.total_progress,
         assignment_progress: progress.assignment_progress,
         instructions_progress: progress.instructions_progress,
@@ -43,12 +47,43 @@ class TrackingSerializer < ActiveModel::Serializer
     end
   end
 
-  # def units
-  #   object.student_week_progresses.reduce([]) {
-  #     |acc, curr|
-  #     if curr.week.unit.unit_name
-  #   }
-  # end
+  def units
+    progress = object.student_weekly_progresses.reduce({}) do |acc, curr|
+      unit = curr.week.unit.unit_name.to_sym
+      if acc.key?(unit)
+        {**acc, unit => [*acc[unit], {
+          week: curr.week_number,
+          week_id: curr.week.id,
+          unit_id: curr.week.unit.id,
+          unit: curr.week.unit.unit_name,
+          unit_description: curr.week.unit.description,
+          lesson_id: curr.week.lesson.id,
+          lesson: curr.week.lesson.lesson_name,
+          total_progress: curr.total_progress,
+          assignment_progress: curr.assignment_progress,
+          instructions_progress: curr.instructions_progress,
+          resources_progress: curr.resources_progress,
+          assignment_submission: curr.assignment_submission
+        }]}
+      else
+        {**acc, unit => [{
+          week: curr.week_number,
+          week_id: curr.week.id,
+          unit_id: curr.week.unit.id,
+          unit: curr.week.unit.unit_name,
+          unit_description: curr.week.unit.description,
+          lesson_id: curr.week.lesson.id,
+          lesson: curr.week.lesson.lesson_name,
+          total_progress: curr.total_progress,
+          assignment_progress: curr.assignment_progress,
+          instructions_progress: curr.instructions_progress,
+          resources_progress: curr.resources_progress,
+          assignment_submission: curr.assignment_submission
+        }]}
+      end
+    end
+    progress
+  end
 
   def course
     object.student_weekly_progresses.map do |progress|
