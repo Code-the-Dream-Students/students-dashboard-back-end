b3cb450802b93c497c432759a0603cabf8034b1b
Jorge Gabitto <jgabitto1792@gmail.com>
2021-02-03 08:53:24 -0600

    added course info to tracking_serializer

--- app/serializers/tracking_serializer.rb
+++ app/serializers/tracking_serializer.rb
@@ -1,5 +1,5 @@
 class TrackingSerializer < ActiveModel::Serializer
-  attributes :message, :student_info
+  attributes :student_id, :course_id, :course, :first_name, :last_name, :student_weekly_progress
 
   # has_many :student_weekly_progresses
   # belongs_to :week
@@ -8,25 +8,58 @@ class TrackingSerializer < ActiveModel::Serializer
     message = "Success"
   end
   
-  def student_info
-    {student_id: object.id, first_name: object.first_name, last_name: object.last_name, course_info: object.student_weekly_progresses[0].week}
+  # def student_info
+  #   {student_id: object.id, first_name: object.first_name, last_name: object.last_name}
+  # end
+
+  def first_name
+    object.first_name
   end
 
-  def course_info
-    object.object
+  def last_name
+    object.last_name
   end
 
-  # def course_info
-  #   { student_weekly_progresses: [
-  #     { 
-  #       total_progress: object.total_progress,
-  #       assignment_progress: object.assignment_progress,
-  #       resources_progress: object.resources_progress,
-  #       instructions_progress: object.instructions_progress,
-  #       assignment_submission: object.assignment_submission
-  #     }
-  #   ]
+  def student_id
+    object.id
+  end
+
+  def student_weekly_progress
+    object.student_weekly_progresses.map do |progress|
+      {
+        unit_id: progress.week.unit.id,
+        unit: progress.week.unit.unit_name,
+        unit_description: progress.week.unit.description,
+        lesson_id: progress.week.lesson.id,
+        lesson: progress.week.lesson.lesson_name,
+        week: progress.week_number,
+        week_id: progress.week.id,
+        total_progress: progress.total_progress,
+        assignment_progress: progress.assignment_progress,
+        instructions_progress: progress.instructions_progress,
+        resources_progress: progress.resources_progress,
+        assignment_submission: progress.assignment_submission
+      }
+    end
+  end
+
+  # def units
+  #   object.student_week_progresses.reduce([]) {
+  #     |acc, curr|
+  #     if curr.week.unit.unit_name
   #   }
-  # end 
+  # end
+
+  def course
+    object.student_weekly_progresses.map do |progress|
+      return progress.week.course.course_name
+    end
+  end
+
+  def course_id
+    object.student_weekly_progresses.map do |progress|
+      return progress.week.course.id
+    end
+  end
 
 end
