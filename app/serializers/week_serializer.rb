f91ebff17a89f911675bf58e4bddb0bedd7dae46
Jorge Gabitto <jgabitto1792@gmail.com>
2021-02-21 14:26:29 -0600

    added course_name and unit_name to week_serializer

--- app/serializers/week_serializer.rb
+++ app/serializers/week_serializer.rb
@@ -1,7 +1,15 @@
 class WeekSerializer < ActiveModel::Serializer
-  attributes :id, :week_number, :course_id, :unit_id, :lesson_id, :start_date, :end_date
+  attributes :id, :week_number, :course_id, :course_name, :unit_id, :unit_name, :lesson_id, :start_date, :end_date
 
   belongs_to :course
   belongs_to :unit
   belongs_to :lesson
+
+  def course_name
+    object.course.course_name
+  end
+
+  def unit_name
+    object.unit.unit_name
+  end
 end
