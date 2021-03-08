859e6c005804a599585aa7dfca92c11c8f4045b1
andresochoa91 <andres_ochoa91@hotmail.com>
2021-03-08 12:43:27 -0800

    Sending user data from session controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,31 +30,31 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(
-      :username,
-      :email,
-      :role,
-      :password,
-      :password_confirmation
-    )
-  end
-
-  def auth_header
-    request.headers['Authorization']
-  end
-
-  def current_user
-    if auth_header
-      decoded_token = CoreModules::JsonWebToken.decode(auth_header)
-      if decoded_token
-        @user = User.find_by(id: decoded_token[:user_id])
-      end
-      if @user then return @user else return false end
+    def user_params
+      params.require(:user).permit(
+        :username,
+        :email,
+        :role,
+        :password,
+        :password_confirmation
+      )
     end
-  end
 
-  def user_options
-    ['user', 'student', 'staff', 'student.student_course.course', 'student.student_weekly_progresses.registered_mentor_sessions', 'student.student_weekly_progresses.week', 'student.student_weekly_progresses.registered_mentor_sessions.mentor_course', 'student.student_weekly_progresses.registered_mentor_sessions.mentor_course.mentor', 'student.student_weekly_progresses.registered_mentor_sessions.mentor_course.mentor.user', 'student.student_weekly_progresses.week.course', 'student.student_weekly_progresses.week.unit', 'student.student_weekly_progresses.week.lesson']
-  end
+    def auth_header
+      request.headers['Authorization']
+    end
+
+    def current_user
+      if auth_header
+        decoded_token = CoreModules::JsonWebToken.decode(auth_header)
+        if decoded_token
+          @user = User.find_by(id: decoded_token[:user_id])
+        end
+        if @user then return @user else return false end
+      end
+    end
+
+    def user_options
+      ['user', 'student', 'staff', 'student.student_course.course', 'student.student_weekly_progresses.registered_mentor_sessions', 'student.student_weekly_progresses.week', 'student.student_weekly_progresses.registered_mentor_sessions.mentor_course', 'student.student_weekly_progresses.registered_mentor_sessions.mentor_course.mentor', 'student.student_weekly_progresses.registered_mentor_sessions.mentor_course.mentor.user', 'student.student_weekly_progresses.week.course', 'student.student_weekly_progresses.week.unit', 'student.student_weekly_progresses.week.lesson']
+    end
 end
