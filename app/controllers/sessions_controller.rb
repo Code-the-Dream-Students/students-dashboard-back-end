1b44a3755743f20006abd880ec826ea5282aee02
jgabitto <jgabitto1792@gmail.com>
2021-03-17 09:58:03 -0500

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,22 +28,20 @@ class SessionsController < Devise::SessionsController
   def create_github
     uri1 = URI('https://github.com/login/oauth/access_token')
 
-    p Rails.application.credentials.fetch(:client_id)
-    p Rails.application.credentials.fetch(:client_secret)
-
     response = Net::HTTP.post_form(uri1, 'client_id' => Rails.application.credentials.fetch(:client_id), 'client_secret' => Rails.application.credentials.fetch(:client_secret), 'code' => params[:code])
     data = CGI::parse(response.body)
-    p data
+    
     token = data.values[0]
-    p token[0]
+    
     headers = {
         "Authorization" => "token #{token[0]}",
         "User-Agent" => "jgabitto"
     }
     # info = HTTParty.get('https://api.github.com/user', :headers => headers)
     # p info
-    email = HTTParty.get('https://api.github.com/user/emails', :headers => headers)
-    p email
+    res = HTTParty.get('https://api.github.com/user/emails', :headers => headers)
+    p res
+    p res.email
 
     redirect_to "http://localhost:3000/"
   end
